a6d85ecfc664c4af52d9589960162be14215c673
Harshwardhan Patil <patil.04042003@gmail.com>
2024-01-10 19:30:54 +0530

    Included app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // require('dotenv').config({path : './env'})
-
+import app from './app.js';
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -8,4 +8,13 @@ import connectDB from "./db/index.js";
 
 
 
-connectDB()
+connectDB().then(
+app.listen(process.env.PORT || 3000, ()=>{
+console.log('connection started at port' + process.env.PORT)
+})
+
+
+)
+.catch((err)=>{
+    console.log("some error occoured in the conncection" + err);
+})
